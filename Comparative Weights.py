d43d4cb555187c70ea064ddb143fabd35693ca89
Jakub Demko <102408363+junglissta@users.noreply.github.com>
2022-12-27 16:36:41 +0100

    Update Comparative Weights.py

--- Comparative Weights.py
+++ Comparative Weights.py
@@ -1,12 +1,22 @@
 def scale_of_truth_n(n):
+  # Check if n is less than 1
   if n < 1:
+    # Return an error message if n is less than 1
     return "Error"
   else:
+    # Initialize a count variable
     count = 0
+    # While n is not equal to 1
     while n != 1:
+      # Keep incrementing n until it is divisible by 3
       while n % 3 != 0:
         n += 1
+      # Divide n by 3
       n /= 3
+      # Increment the count variable
       count += 1
+    # Return the final count
     return count
+
+# Test the function with a negative input
 scale_of_truth_n(-2)
