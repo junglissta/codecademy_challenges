6182785a340ce39d2aebc726f7584fff1ed6a5fb
Jakub Demko <102408363+junglissta@users.noreply.github.com>
2022-12-27 16:48:22 +0100

    Update fib_find.py

--- fib_find.py
+++ fib_find.py
@@ -1,9 +1,12 @@
 def fib_finder(n):
-  # Write your code here
+  # Initialize variables to store the current and next fibonacci number
   begin = 0
   next = 1
   for i in range(n-1):
+    # Calculate the next fibonacci number by adding the current and next numbers
     begin = begin + next
+    # Update the current number to be the previous next number
     next = begin - next
+  # Return the current fibonacci number, which is the nth number in the sequence
   return begin
 print(fib_finder(100))
