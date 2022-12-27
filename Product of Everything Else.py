9824e2d484c90fa05155b01952902ac18c3aa7c7
Jakub Demko <102408363+junglissta@users.noreply.github.com>
2022-12-27 16:37:50 +0100

    Update Product of Everything Else.py

--- Product of Everything Else.py
+++ Product of Everything Else.py
@@ -1,19 +1,24 @@
-# import numpy
 def product_of_the_others(array):
+  # Create an empty list to store the products
   final_list = []
+  # Iterate through each element in the array
   for i in array:
+    # Create a new list which is a copy of the array
     new_list = [x for x in array]
+    # Remove the element at the current index from the new list
     new_list.remove(i)
-    # final_list.append(numpy.prod(new_list))
+    # Initialize a variable to store the product of the elements in the new list
     prod = 1
+    # Iterate through each element in the new list
     for num in new_list:
+      # Multiply the product by the current element
       prod = prod * num
-
+    # Append the product to the final list
     final_list.append(prod)
+    # Clear the elements in the new list
     new_list.clear()
+  # Return the final list of products
   return final_list
-      
-  print(final_list)
 
 
 print(product_of_the_others([1, 2, 3, 4, 5]))
